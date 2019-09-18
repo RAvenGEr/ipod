17d55ed9ab3b27172ea3978c5d7837ff75f67082
David Webb <david@dpwlabs.com>
2019-09-18 19:57:29 +0930

    Squashed commit of the following:
    commit a0e1425c0986118d9fe9f04b7c741e5247df5dc9
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Sun Mar 24 18:03:04 2019 +0200
    
        cleanup PlayControl handler
    
    commit 05632f08758c07d93c02b345ca04d53cae1b7862
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Sun Mar 24 18:01:39 2019 +0200
    
        return track index of -1 if nothing is playing
    
    commit 7a959d6a5300758c88694e18d33048043f48ff19
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Thu Mar 7 08:41:45 2019 +0200
    
        fix: more track metadata
    
    commit 48e3e06dfe133788c8c04eef309acc133bdbb979
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Wed Mar 6 22:16:02 2019 +0200
    
        WIP: fix track info according to the spec
    
    commit 93f27c37fe20ae89df0e2cefa4740df785e1d621
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Wed Mar 6 22:14:22 2019 +0200
    
        better dbus connection error handling
    
    commit a33ef1cdbba6a25126a1f5a8e7d3b674bd1e4f09
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Wed Mar 6 22:10:27 2019 +0200
    
        WIP: track metadata
    
    commit 535563ac3bf2849d668c9c5494ce69006d73ea34
    Author: Ari Croock <aricroock@gmail.com>
    Date:   Wed Mar 6 22:09:58 2019 +0200
    
        WIP: dbus functions for accessory button controls
    
    commit 510d9e9bbe1005fbd9d1c2cd064ac8d33bed9b04
    Author: Ari Croock <acroock@isaziconsulting.co.za>
    Date:   Tue Aug 21 21:06:39 2018 +0200
    
        playing status and num tracks

--- lingo-extremote/handler.go
+++ lingo-extremote/handler.go
@@ -1,7 +1,14 @@
 package extremote
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/oandrew/ipod"
+
+	"github.com/godbus/dbus"
+
+	"log"
 )
 
 type DeviceExtRemote interface {
@@ -16,7 +23,57 @@ func ackSuccess(req *ipod.Command) *ACK {
 // 	return ACKPending{Status: ACKStatusPending, CmdID: uint8(req.ID.CmdID()), MaxWait: maxWait}
 // }
 
+func getTrackMetadata(connection *dbus.Conn, playerPath dbus.ObjectPath) (map[string]interface{}, error) {
+	if playerPath == "" {
+		return nil, errors.New("playerPath is empty")
+	}
+
+	player := connection.Object("org.bluez", playerPath)
+	track, err := player.GetProperty("org.bluez.MediaPlayer1.Track")
+	if err != nil {
+		return nil, errors.New("could not get track from dbus: " + err.Error())
+	}
+
+	t, ok := track.Value().(map[string]dbus.Variant)
+	if ok {
+		return map[string]interface{}{"title": t["Title"].String(), "artist": t["Artist"].String(), "album": t["Album"].Value().(string)}, nil
+	}
+	return nil, errors.New("Could not coerce track to map")
+}
+
+func getPlayerStatus(connection *dbus.Conn, playerPath dbus.ObjectPath) (string, error) {
+	if playerPath == "" {
+		return "", errors.New("playerPath is empty")
+	}
+
+	player := connection.Object("org.bluez", playerPath)
+	status, err := player.GetProperty("org.bluez.MediaPlayer1.Status")
+	if err != nil {
+		return "", errors.New("could not get player status from dbus: " + err.Error())
+	}
+
+	return status.Value().(string), nil
+}
+
 func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemote) error {
+	var playerPath dbus.ObjectPath
+	conn, err := dbus.SystemBus()
+	if err == nil {
+		var objects map[dbus.ObjectPath]map[string]map[string]dbus.Variant
+		conn.Object("org.bluez", "/").Call("org.freedesktop.DBus.ObjectManager.GetManagedObjects", 0).Store(&objects)
+
+		for path, obj := range objects {
+			for inter := range obj {
+				if inter == "org.bluez.MediaPlayer1" {
+					playerPath = path
+					break
+				}
+			}
+		}
+	}
+
+	log.Printf("playerPath: %v", playerPath)
+
 	//log.Printf("Req: %#v", req)
 	switch msg := req.Payload.(type) {
 
@@ -51,17 +108,26 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 				TrackLength:  300 * 1000,
 				ChapterCount: 0,
 			}
-		case TrackInfoDescription, TrackInfoLyrics:
+		case TrackInfoArtistName:
+			info = ipod.StringToBytes("GetIndexedPlayingTrackInfo ArtistName")
+		case TrackInfoAlbum:
+			info = ipod.StringToBytes("GetIndexedPlayingTrackInfo Album")
+		case TrackInfoGenre:
+			info = ipod.StringToBytes("GetIndexedPlayingTrackInfo Genre")
+		case TrackInfoTitle:
+			info = ipod.StringToBytes("GetIndexedPlayingTrackInfo Title")
+		case TrackInfoComposer:
+			info = ipod.StringToBytes("GetIndexedPlayingTrackInfo Composer")
+		case TrackInfoArtworkCount:
+			info = struct{}{}
+		case TrackInfoLyrics:
 			info = &TrackLongText{
 				Flags:       0x0,
 				PacketIndex: 0,
 				Text:        0x00,
 			}
-		case TrackInfoArtworkCount:
-			info = struct{}{}
 		default:
-			info = []byte{0x00}
-
+			info = ipod.StringToBytes("WAT")
 		}
 		ipod.Respond(req, tr, &ReturnIndexedPlayingTrackInfo{
 			InfoType: msg.InfoType,
@@ -79,32 +145,93 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 	case *SelectDBRecord:
 		ipod.Respond(req, tr, ackSuccess(req))
 	case *GetNumberCategorizedDBRecords:
-		ipod.Respond(req, tr, &ReturnNumberCategorizedDBRecords{
-			RecordCount: 1,
-		})
+		switch msg.CategoryType {
+		case DbCategoryTrack:
+			ipod.Respond(req, tr, &ReturnNumberCategorizedDBRecords{
+				RecordCount: 10,
+			})
+		default:
+			ipod.Respond(req, tr, &ReturnNumberCategorizedDBRecords{
+				RecordCount: 0,
+			})
+		}
 	case *RetrieveCategorizedDatabaseRecords:
+		if msg.CategoryType == DbCategoryTrack {
+			var arr [16]byte
+			copy(arr[:], ipod.StringToBytes(fmt.Sprintf("Track %d", msg.Offset)))
+			ipod.Respond(req, tr, &ReturnCategorizedDatabaseRecord{msg.Offset, arr})
+			return nil
+		}
 		ipod.Respond(req, tr, &ReturnCategorizedDatabaseRecord{})
 	case *GetPlayStatus:
+		var state PlayerState
+		status, err := getPlayerStatus(conn, playerPath)
+		if err != nil {
+			log.Printf("ERROR: getting play status from dbus: " + err.Error())
+			state = PlayerStateStopped
+		} else {
+			log.Printf("play status from dbus: " + status)
+			switch status {
+			case "playing":
+				state = PlayerStatePlaying
+			default:
+				state = PlayerStatePaused
+			}
+		}
 		ipod.Respond(req, tr, &ReturnPlayStatus{
+			State:         state,
+			TrackIndex:    0,
 			TrackLength:   300 * 1000,
 			TrackPosition: 20 * 1000,
-			State:         PlayerStatePaused,
 		})
 	case *GetCurrentPlayingTrackIndex:
-		ipod.Respond(req, tr, &ReturnCurrentPlayingTrackIndex{
-			TrackIndex: 0,
-		})
+		if playerPath == "" {
+			ipod.Respond(req, tr, &ReturnCurrentPlayingTrackIndex{
+				TrackIndex: -1,
+			})
+		} else {
+			ipod.Respond(req, tr, &ReturnCurrentPlayingTrackIndex{
+				TrackIndex: 0,
+			})
+		}
 	case *GetIndexedPlayingTrackTitle:
+		var title string
+		track, err := getTrackMetadata(conn, playerPath)
+		if err != nil {
+			log.Printf("ERROR: getting track title: %s", err.Error())
+			title = err.Error()
+		} else {
+			title = track["title"].(string)
+			log.Printf("got track title: %s", title)
+		}
 		ipod.Respond(req, tr, &ReturnIndexedPlayingTrackTitle{
-			Title: ipod.StringToBytes("title"),
+			Title: ipod.StringToBytes(title),
 		})
 	case *GetIndexedPlayingTrackArtistName:
+		var artist string
+		track, err := getTrackMetadata(conn, playerPath)
+		if err != nil {
+			log.Printf("ERROR: getting track artist: %s", err.Error())
+			artist = err.Error()
+		} else {
+			artist = track["artist"].(string)
+			log.Printf("got track artist: %s", artist)
+		}
 		ipod.Respond(req, tr, &ReturnIndexedPlayingTrackArtistName{
-			ArtistName: ipod.StringToBytes("artist"),
+			ArtistName: ipod.StringToBytes(artist),
 		})
 	case *GetIndexedPlayingTrackAlbumName:
+		var album string
+		track, err := getTrackMetadata(conn, playerPath)
+		if err != nil {
+			log.Printf("ERROR: getting track album: %s", err.Error())
+			album = err.Error()
+		} else {
+			album = track["album"].(string)
+			log.Printf("got track album: %s", album)
+		}
 		ipod.Respond(req, tr, &ReturnIndexedPlayingTrackAlbumName{
-			AlbumName: ipod.StringToBytes("album"),
+			AlbumName: ipod.StringToBytes(album),
 		})
 	case *SetPlayStatusChangeNotification:
 		ipod.Respond(req, tr, ackSuccess(req))
@@ -113,7 +240,89 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 	case *PlayCurrentSelection:
 		ipod.Respond(req, tr, ackSuccess(req))
 	case *PlayControl:
-		ipod.Respond(req, tr, ackSuccess(req))
+		if playerPath == "" {
+			break
+		}
+
+		payload := req.Payload.(*PlayControl)
+		var err error
+
+		switch payload.Cmd {
+		case PlayControlToggle:
+			status, err := getPlayerStatus(conn, playerPath)
+			log.Printf("Play status from dbus: " + status)
+			if err != nil {
+				log.Printf("ERROR: getting play status from dbus: %s", err.Error())
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Play: %v", err)
+				}
+				break
+			}
+
+			switch status {
+			case "playing":
+				log.Printf("current status is '%s' calling DBus '%s'", status, "Pause")
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Pause", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Pause: %v", err)
+				}
+			case "stopped":
+				log.Printf("current status is '%s' calling DBus '%s'", status, "Play")
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Play: %v", err)
+				}
+			case "paused":
+				log.Printf("current status is '%s' calling DBus '%s'", status, "Play")
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Play: %v", err)
+				}
+			default:
+				log.Printf("current status is '%s' calling DBus '%s'", status, "Play")
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Play: %v", err)
+				}
+			}
+
+		case PlayControlPlay:
+			log.Print("Play")
+			err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+			if err != nil {
+				log.Printf("ERROR: calling Play: %v", err)
+			}
+		case PlayControlPause:
+			log.Print("Pause")
+			err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Pause", 0).Store()
+			if err != nil {
+				log.Printf("ERROR: calling Pause: %v", err)
+			}
+		case PlayControlNextTrack, PlayControlNextChapter, PlayControlNext:
+			log.Print("Next")
+			err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Next", 0).Store()
+			if err != nil {
+				log.Printf("ERROR: calling Next: %v", err)
+			}
+		case PlayControlPrevTrack, PlayControlPrevChapter, PlayControlPrev:
+			log.Print("Prev")
+			err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Previous", 0).Store()
+			if err != nil {
+				log.Printf("ERROR: calling Previous: %v", err)
+			}
+		}
+
+		if err != nil {
+			log.Printf("ERROR: PlayControl (%v): %s", payload.Cmd, err.Error())
+			ipod.Respond(req, tr, &ACK{Status: ACKStatusFailed, CmdID: req.ID.CmdID()})
+		} else {
+			// TODO: not sure if the ACK status should be 0 (success) or the actual control status?
+			log.Printf("Responding with ACKStatus: %v (%d)", ACKStatus(payload.Cmd), payload.Cmd)
+			// ipod.Respond(req, tr, &ACK{Status: ACKStatusSuccess, CmdID: req.ID.CmdID()})
+			ipod.Respond(req, tr, &ACK{Status: ACKStatus(payload.Cmd), CmdID: req.ID.CmdID()})
+		}
+
 	case *GetTrackArtworkTimes:
 		ipod.Respond(req, tr, &RetTrackArtworkTimes{})
 	case *GetShuffle:
@@ -135,9 +344,15 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 			PixelFormat: 0x01,
 		})
 	case *GetNumPlayingTracks:
-		ipod.Respond(req, tr, &ReturnNumPlayingTracks{
-			NumTracks: 0,
-		})
+		if playerPath != "" {
+			ipod.Respond(req, tr, &ReturnNumPlayingTracks{
+				NumTracks: 10,
+			})
+		} else {
+			ipod.Respond(req, tr, &ReturnNumPlayingTracks{
+				NumTracks: 0,
+			})
+		}
 	case *SetCurrentPlayingTrack:
 	case *SelectSortDBRecord:
 	case *GetColorDisplayImageLimits:
@@ -147,7 +362,12 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 			PixelFormat: 0x01,
 		})
 	case *ResetDBSelectionHierarchy:
-		ipod.Respond(req, tr, &ACK{Status: ACKStatusFailed, CmdID: req.ID.CmdID()})
+		switch msg.Selection {
+		case 1:
+			ipod.Respond(req, tr, &ACK{Status: ACKStatusSuccess, CmdID: req.ID.CmdID()})
+		default:
+			ipod.Respond(req, tr, &ACK{Status: ACKStatusFailed, CmdID: req.ID.CmdID()})
+		}
 
 	case *GetDBiTunesInfo:
 	// RetDBiTunesInfo:
